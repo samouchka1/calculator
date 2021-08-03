75e6796d0fefe21a45bf23ac98b9d8643f69b8d0
samouchka1 <82068124+samouchka1@users.noreply.github.com>
2021-08-03 10:59:16 -0400

    Update script.js

--- calculator/script.js
+++ calculator/script.js
@@ -6,32 +6,41 @@ function oneF() {document.getElementById("result").textContent = "1";}
 const one = document.getElementById("1");
 one.addEventListener("click", oneF);
 
-
+function twoF() {document.getElementById("result").textContent = "2";}
 const two = document.getElementById("2");
+two.addEventListener("click", twoF);
 
-
+function threeF() {document.getElementById("result").textContent = "3";}
 const three = document.getElementById("3");
+three.addEventListener("click", threeF);
 
-
+function fourF() {document.getElementById("result").textContent = "4";}
 const four = document.getElementById("4");
+four.addEventListener("click", fourF);
 
-
+function fiveF() {document.getElementById("result").textContent = "5";}
 const five = document.getElementById("5");
+five.addEventListener("click", fiveF);
 
-
+function sixF() {document.getElementById("result").textContent = "6";}
 const six = document.getElementById("6");
+six.addEventListener("click", sixF);
 
-
+function sevenF() {document.getElementById("result").textContent = "7";}
 const seven = document.getElementById("7");
+seven.addEventListener("click", sevenF);
 
-
+function eightF() {document.getElementById("result").textContent = "8";}
 const eight = document.getElementById("8");
+eight.addEventListener("click", eightF);
 
-
+function nineF() {document.getElementById("result").textContent = "9";}
 const nine = document.getElementById("9");
+nine.addEventListener("click", nineF);
 
-
+function decF() {document.getElementById("result").textContent = ".";}
 const decimal = document.getElementById("dec");
+decimal.addEventListener("click", decF);
 
 //operators
 
