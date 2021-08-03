d2ee4987be6e28aa3fe317b5e37d28a75972e937
samouchka1 <82068124+samouchka1@users.noreply.github.com>
2021-08-02 23:06:58 -0400

    Add files via upload

---
+++ calculator/script.js
@@ -0,0 +1,57 @@
+function zeroF() {document.getElementById("result").textContent = "0";}
+const zero = document.getElementById("0");
+zero.addEventListener("click", zeroF);
+
+function oneF() {document.getElementById("result").textContent = "1";}
+const one = document.getElementById("1");
+one.addEventListener("click", oneF);
+
+
+const two = document.getElementById("2");
+
+
+const three = document.getElementById("3");
+
+
+const four = document.getElementById("4");
+
+
+const five = document.getElementById("5");
+
+
+const six = document.getElementById("6");
+
+
+const seven = document.getElementById("7");
+
+
+const eight = document.getElementById("8");
+
+
+const nine = document.getElementById("9");
+
+
+const decimal = document.getElementById("dec");
+
+//operators
+
+
+const divide = document.getElementById("divide");
+
+
+const multiply = document.getElementById("multiply");
+
+
+const add = document.getElementById("add");
+
+
+const subtract = document.getElementById("subtract");
+
+
+//buttons
+
+function clearArea() {document.getElementById("result").textContent = "";}
+const clear = document.getElementById("clear");
+clear.addEventListener("click", clearArea);
+
+const equal = document.getElementById("equal");
